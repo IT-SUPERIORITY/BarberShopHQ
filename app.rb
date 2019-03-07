d2d518dabf2feec562b42304aaf6618864c446f0
Michael Yaschenko <mikhail_yaschenko@mail.ru>
2019-03-07 13:56:31 +0200

    changed to a barbershop

--- app.rb
+++ app.rb
@@ -2,8 +2,9 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
 
-set :database, "sqlite3.my_database.db"
+set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
 end
